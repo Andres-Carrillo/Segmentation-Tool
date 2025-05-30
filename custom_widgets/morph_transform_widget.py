3adf2a56b10106b300a1234fa8aacaf6dd21549e
andres <andres.carrillo@appliedmedical.com>
2025-05-30 00:42:34 -0700

    Updates to widget for handling cv dialation and erosion.

--- custom_widgets/morph_transform_widget.py
+++ custom_widgets/morph_transform_widget.py
@@ -1,75 +1,84 @@
-from PyQt5.QtWidgets import ( QWidget,QVBoxLayout,QStackedLayout,QDial)
+from PyQt5.QtWidgets import (QWidget)
 from PyQt5 import QtWidgets
 from PyQt5 import QtCore
-# from PyQt5 import QtGui
 from PyQt5.QtGui import QPainter, QPen, QColor, QBrush
-from custom_widgets.range_widget import RangeSlider
+from utils import calculate_angle_between,calculate_poin_along_arc,in_circle
 
 
-class QDialMarked(QWidget):
-    def __init__(self, parent = None, kernel_size=5, iterations=1, flags=None):
+class Gauge(QWidget):
+    def __init__(self, parent = None, kernel_size=5, iterations=1, background_color=QColor(217, 217, 206), handle_color=QColor(255, 255, 255), outline_color=QColor(0, 0, 0)):
         super().__init__(parent)
         self.kernel_size = kernel_size
         self.iterations = iterations
         self.max = 7
         self.min = 1
 
-    def paintEvent(self, event):
-        # draw normal dial
-        super().paintEvent(event)
+        self.background_color = background_color
+        self.handle_color = handle_color
+        self.outline_color = outline_color
+        self.dragging = False
+        self.setMouseTracking(True)
+        self.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        
+        self.setMinimumSize(200, 200)
         self.setStyleSheet("background-color: None;")
 
-        self.setMinimumSize(200, 200)
-        self.setMaximumSize(200, 200)
-        # self.setValue(self.kernel_size)
+        # rectangles used to draw the inner and outer edges of the dial
+        self.inner_ring = self.rect().adjusted(20, 20, -20, -20)
+        self.outer_ring = self.rect().adjusted(40, 40, -40, -40)
+        self.handle_track = self.rect().adjusted(30, 30, -30, -30)
+        
+        # rectangles used to draw the endcaps of the dial
+        self.start_cap = QtCore.QRectF(20, 90, 20, 20)
+        self.end_cap = QtCore.QRectF(160, 90, 20, 20)
 
-        # # now draw tick marks along the dial
+        self.handle_center = self.start_cap.center()
+        self.handle_size = 10
+
+    def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-        painter.setPen(QPen(QColor(0, 0, 0), 2))
-        painter.setBrush(QBrush(QColor(0, 0, 0)))
-        painter.drawEllipse(self.rect().center(), 10, 10)
 
-        # draw arch for the dial
-        painter.setPen(QPen(QColor(0, 0, 0), 2))
-        painter.setBrush(QBrush(QColor(255, 255, 255)))
+        # draw background of the dial
+        painter.setPen(QPen(self.background_color, 20))
+        painter.drawArc(self.handle_track, 0, 180 * 16)
         
-        inner_ring = self.rect().adjusted(20, 20, -20, -20)
-        outer_ring = self.rect().adjusted(40, 40, -40, -40)
-        painter.drawArc(inner_ring, 0, 180 * 16)
-        painter.drawArc(outer_ring, 0, 180 * 16)
+        # set painter color and brush for the inner and outer arcs
+        painter.setPen(QPen(self.outline_color, 2))      
 
-        #draw edge cap of the dial
-        # painter.setPen(QPen(QColor(0, 255, 255), 2))
-        # painter.setBrush(QBrush(QColor(0, 0, 0)))
-        # draw arc for the endcap of the dial
-        # painter.drawArc(inner_ring, 0, 180 * 16)
-        # painter.drawArc(outer_ring, 0, 180 * 16)
+        # draw the outline of the dial
+        painter.drawArc(self.inner_ring, 0, 180 * 16)
+        painter.drawArc(self.outer_ring, 0, 180 * 16)
 
-        print("innter ring", inner_ring)
-        print("outer ring", outer_ring)
-        #draw arch that goes from the start of the inner ring to the start of the outer ring
-        # encap = self.rect().adjusted(20, 160, -140, -80)
-        start_cap = QtCore.QRectF(20, 90, 20, 20)
-        end_cap = QtCore.QRectF(160, 90, 20, 20)
-        # encap = self.rect().adjusted(20, 20, -20, -20)
+        # draw the endcaps of each arc
+        painter.drawArc(self.start_cap, 0, -180 * 16)
+        painter.drawArc(self.end_cap, 0, -180 * 16)
 
-        # print("encap", encap)
+        # draw the handle
+        painter.setBrush(QBrush(self.handle_color))
+        painter.drawEllipse(self.handle_center,self.handle_size, self.handle_size)
 
-        # painter.translate(QtCore.QPointF(encap.x(),encap.y()))
-
-        #draw the rectangle around the endcap
-        # painter.setPen(QPen(QColor(0, 255, 255), 2))
-        # painter.setBrush(QBrush(QColor(0, 0, 0)))
-
-        # painter.rotate(360)
-        # painter.drawRect(encap)
-        painter.drawArc(start_cap, 0, -180 * 16)
-        painter.drawArc(end_cap, 0, -180 * 16)
-        # painter.drawEllipse(self.rect().center(), 10, 10)
-        # draw tick marks
-
-
-        # painter.drawText(self.rect().center(), QtCore.Qt.AlignCenter, str(self.kernel_size))
         painter.end()
         
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+                clicked_handle = in_circle(self.handle_center.x(),self.handle_center.y(), event.pos().x(), event.pos().y(), self.handle_size)
+                
+                if clicked_handle:
+                    self.dragging = True
+              
+    def mouseReleaseEvent(self, event):
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            self.dragging = False
+        
+    def mouseMoveEvent(self, event):
+            center = self.handle_track.center()
+            angle = calculate_angle_between((center.x(),center.y()), (event.pos().x(), event.pos().y()))
+
+            if (angle < 1 or angle > 178) and self.dragging:
+            
+                point = calculate_poin_along_arc((center.x(),center.y()), int(self.handle_track.width()/2),angle)
+
+                self.handle_center = QtCore.QPoint(point[0], point[1])
+
+                self.update()
